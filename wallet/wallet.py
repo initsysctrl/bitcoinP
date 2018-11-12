d6ef6e30618b40c36e8e810feaff207c87dcb5c3
yepeng <initsysctrl@gmail.com>
2018-11-12 19:07:04 +0800

    修复bug

--- wallet/wallet.py
+++ wallet/wallet.py
@@ -20,23 +20,23 @@ import crypto
 
 
 class Wallet(object):
+
     def __init__(self, privatea_key_hex=None, base58private=None, wifprivate=None, anyword=None):
         if privatea_key_hex is not None:
             self.private_key = privatea_key_hex
+        elif base58private is not None:
+            self.private_key = crypto.base58decode(base58private)
         elif wifprivate is not None:
             self.private_key = crypto.base58_check_decode(base58private)
         elif anyword is not None:
             self.private_key = hashlib.sha256(bytes(anyword, 'utf-8')).digest().hex()
-
-        self.private_key = hex(secrets.randbits(256))[2:] if privatea_key_hex is None else privatea_key_hex
-        print(self.private_key)
+        else:
+            self.private_key = hex(secrets.randbits(256))[2:]
         public_key = self._creat_publickey(self.private_key)
         self.public_key_compress = public_key[0]
         self.public_key_uncompress = public_key[1]
         self.address = self._creat_address(self.public_key_compress)
-
-    def __str__(self):
-        return self.__dict__
+        self.address_un = self._creat_address(self.public_key_uncompress)
 
     # 私钥生成公钥
     @classmethod
@@ -80,6 +80,13 @@ class Wallet(object):
     # 导出压缩WIF格式的私钥
     def export_compress_privatekey(self) -> str:
         return crypto.base58_check_encode(0x80, self.private_key + '01')
+
+    def __str__(self):
+        return 'private key(HEX):' + self.private_key + '\n' \
+               + 'public key(compress):' + self.public_key_compress + '\n' \
+               + 'public key (uncompress):' + self.public_key_uncompress + '\n' \
+               + 'address(C):' + self.address + '\n' \
+               + 'address(U):' + self.address_un
 
 
 if __name__ == '__main__':
